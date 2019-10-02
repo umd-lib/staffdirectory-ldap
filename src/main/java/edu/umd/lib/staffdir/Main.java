73bed127a3263dd47aed35f897ebe250bcdc33b4
David P. Steelman <dsteelma@umd.edu>
2019-10-02 09:09:50 -0400

    LIBWEB-4973. Initial working implementation
    "Main" class now produces a spreadsheet using the information from
    a Google Sheets document and LDAP.
    
    https://issues.umd.edu/browse/LIBWEB-4973

--- src/main/java/edu/umd/lib/staffdir/Main.java
+++ src/main/java/edu/umd/lib/staffdir/Main.java
@@ -3,9 +3,13 @@ package edu.umd.lib.staffdir;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
+import java.util.Set;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -18,6 +22,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import edu.umd.lib.staffdir.excel.ExcelGenerator;
+import edu.umd.lib.staffdir.google.Organizations;
+import edu.umd.lib.staffdir.google.SheetsRetriever;
 import edu.umd.lib.staffdir.ldap.Ldap;
 
 /**
@@ -68,19 +74,92 @@ public class Main {
       System.exit(1);
     }
 
+    // Google configuration Settings
+    String appName = props.getProperty("appName");
+    String clientSecretFile = props.getProperty("clientSecretFile");
+    String spreadsheetDocId = props.getProperty("spreadsheetDocId");
+    SheetsRetriever sr = new SheetsRetriever(appName, clientSecretFile);
+
+    List<Map<String, String>> rawOrganizationsList = sr.toMap(spreadsheetDocId, "Organization");
+    List<Map<String, String>> rawStaffMap = sr.toMap(spreadsheetDocId, "Staff");
+
+    String costCenterField = "Cost Center";
+    String uidField = "Directory ID";
+    Map<String, Map<String, String>> organizationsMap = Organizations.getCostCentersMap(rawOrganizationsList,
+        costCenterField);
+    Map<String, Map<String, String>> staffMap = createStaffMap(rawStaffMap, uidField);
+
+    Set<String> uids = staffMap.keySet();
+
+    // LDAP configuration settings
     String ldapUrl = props.getProperty("ldap.url");
     String authentication = props.getProperty("ldap.authentication");
     String bindDn = props.getProperty("ldap.bindDn");
     String credentials = props.getProperty("ldap.credentials");
     String searchBaseDn = props.getProperty("ldap.searchBaseDn");
 
-    List<Person> persons = Ldap.ldapSearch(ldapUrl, authentication, bindDn, credentials, searchBaseDn);
+    Ldap ldap = new Ldap(ldapUrl, authentication, bindDn, credentials, searchBaseDn);
+    Map<String, Map<String, String>> ldapResults = ldap.getUsers(uids);
+
+    List<Person> persons = new ArrayList<>();
+    for (String uid : uids) {
+      persons.add(createPerson(uid, staffMap, ldapResults, organizationsMap));
+    }
 
     // Sort the persons by last name
     Collections.sort(persons,
         (Person p1, Person p2) -> p1.getLastName().toLowerCase().compareTo(p2.getLastName().toLowerCase()));
 
     ExcelGenerator.generate(outputFilename, persons, "abcd");
+  }
+
+  public static Person createPerson(String uid,
+      Map<String, Map<String, String>> staffMap,
+      Map<String, Map<String, String>> ldapResults,
+      Map<String, Map<String, String>> organizationsMap) {
+    Map<String, String> staffEntry = staffMap.get(uid);
+    Map<String, String> ldapResult = ldapResults.get(uid);
+    String costCenter = staffEntry.get("Cost Center");
+    Map<String, String> organization = organizationsMap.get(costCenter);
+
+    boolean facultyPermStatus = false;
+    String facultyPermStatusStr = staffEntry.get("Faculty Perm Status");
+    if ((facultyPermStatusStr != null) && ("p".equals(facultyPermStatusStr.toLowerCase()))) {
+      facultyPermStatus = true;
+    }
+
+    PersonBuilder pb = new PersonBuilder();
+    pb.uid(uid)
+        .lastName(ldapResult.get("sn"))
+        .firstName(ldapResult.get("givenName"))
+        .phoneNumber(ldapResult.get("telephoneNumber"))
+        .email(ldapResult.get("mail"))
+        .officialTitle(ldapResult.get("umOfficialTitle"))
+        .jobTitle(ldapResult.get("umDisplayTitle"))
+        .roomNumber(ldapResult.get("umPrimaryCampusRoom"))
+        .building(ldapResult.get("umPrimaryCampusBuilding"))
+        .division(organization.get("Division Code"))
+        .department(organization.get("Department"))
+        .unit(organization.get("Unit"))
+        .fte(staffEntry.get("Appt Fte"))
+        .categoryStatus(ldapResult.get("umCatStatus"))
+        .costCenter(costCenter)
+        .facultyPermanentStatus(facultyPermStatus)
+        .descriptiveTitle(ldapResult.get("umOptionalTitle"))
+        .location(organization.get("Location"));
+
+    return pb.getPerson();
+
+  }
+
+  public static Map<String, Map<String, String>> createStaffMap(List<Map<String, String>> rawStaffMap,
+      String uidField) {
+    Map<String, Map<String, String>> results = new HashMap<>();
+    for (Map<String, String> entry : rawStaffMap) {
+      String uid = entry.get(uidField);
+      results.put(uid, entry);
+    }
+    return results;
   }
 
   /**
