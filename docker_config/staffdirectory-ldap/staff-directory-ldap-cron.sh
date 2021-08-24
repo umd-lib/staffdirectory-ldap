e5a64b56996c823135a9904ee10f5bbc5ba368a9
David P. Steelman <dsteelma@umd.edu>
2021-08-24 13:42:03 -0400

    LIBITD-1976. Initial implementation of Staff Directory LDAP Cron script
    Created "Dockerfile-staffdir-cron" that creates an image suitable for
    running the "staff-directory-ldap-cron.sh" script.
    
    The "staff-directory-ldap-cron" script is the "main" script, that calls
    the "staff-list-excel.sh" script as needed.
    
    https://issues.umd.edu/browse/LIBITD-1976

---
+++ docker_config/staffdirectory-ldap/staff-directory-ldap-cron.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# To support Kubernetes volume mounting, this script uses the following
+# subdirectories of SCRIPT_DIR
+#
+#   output/ - Directory containing output of the scripts. Should be mounted in
+#             persistent volume.
+#   config/ - Directory for providing the configuraton information, such as the
+#             Google service account JSON file, and "config.properties" file
+#             Can be implemented as a Kubernetes configMap so it does not
+#             need to be a persistent volume.
+
+export SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+export CONFIG_PROPERTIES_FILE=$SCRIPT_DIR/config/config.properties
+export LATEST_JSON_FILE=$SCRIPT_DIR/output/persons.json.latest
+export CURRENT_JSON_FILE=$SCRIPT_DIR/output/persons.json
+export BACKUP_JSON_FILE=$SCRIPT_DIR/output/persons.json.bak
+
+CONFIG_FILE=$SCRIPT_DIR/config/config.properties
+
+cd "$SCRIPT_DIR"
+
+echo === Retrieving staff information ===
+bin/staff-retriever --config "$CONFIG_PROPERTIES_FILE" --output "$LATEST_JSON_FILE"
+
+# Exit if previous file exists, and it the same as the newly generated file
+if [ -f "$CURRENT_JSON_FILE" ]; then
+  if diff -q "$CURRENT_JSON_FILE" "$LATEST_JSON_FILE"; then
+     echo "$CURRENT_JSON_FILE" and "$LATEST_JSON_FILE" are the same. Exiting.
+     exit 0
+  fi
+fi
+
+if [ -f "$CURRENT_JSON_FILE" ]; then
+  echo Moving "$CURRENT_JSON_FILE" to "$BACKUP_JSON_FILE"
+  mv "$CURRENT_JSON_FILE" "$BACKUP_JSON_FILE"
+fi
+
+echo Moving "$LATEST_JSON_FILE" to "$CURRENT_JSON_FILE"
+mv $LATEST_JSON_FILE $CURRENT_JSON_FILE
+
+echo === Calling staff-list-excel.sh ===
+./staff-list-excel.sh
+EXCEL_RESULT=$?
+if [[ "$EXCEL_RESULT" -ne "0" ]]; then
+  echo "ERROR: staff-list-excel.sh failed."
+fi
+
+echo === Call staff-list-drupal.sh ===
+echo TODO
---
+++ docker_config/staffdirectory-ldap/staff-list-excel.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Builds the "All Staff List.xslx" Excel spreadsheet and copies it to the
+# "Staff Directory Information" directory on the Library Samba share.
+#
+# The following environment variables are expected by this script:
+#
+#    SMB_USER - the Samba username
+#    SMB_PASSWORD - the Samba password
+
+export SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+export CONFIG_PROPERTIES_FILE=$SCRIPT_DIR/config/config.properties
+JSON_FILE="$SCRIPT_DIR/output/persons.json"
+EXCEL_FILE="$SCRIPT_DIR/output/All Staff List New.xlsx"
+UPLOAD_EXCEL_FILENAME="TEST-All Staff List New-TEST.xlsx"
+
+if [ -z "$SMB_USER" ]; then
+  echo "Please provide a non-empty 'SMB_USER' environment variable"
+  exit 1
+fi
+
+if [ -z "$SMB_PASSWORD" ]; then
+  echo "Please provide a non-empty 'SMB_PASSWORD' environment variable"
+  exit 1
+fi
+
+echo === Building Excel spreadsheet ===
+$SCRIPT_DIR/bin/all-staff-list-builder --config "$CONFIG_PROPERTIES_FILE" --input "$JSON_FILE" --output "$EXCEL_FILE"
+BUILD_RESULT=$?
+if (( $BUILD_RESULT != 0 )); then
+  echo "ERROR: An error occurred running all-staff-list-builder."
+  echo $SCRIPT_DIR/bin/all-staff-list-builder --config "$CONFIG_PROPERTIES_FILE" --input "$JSON_FILE" --output "$EXCEL_FILE"
+  exit 1
+fi
+
+echo === Uploading updated Excel spreadsheet ===
+/usr/bin/smbclient \
+  '//librfs001v.ad.umd.edu/department$' \
+  --user $SMB_USER%$SMB_PASSWORD \
+  -D 'LibraryShare\Staff Directory Information' \
+  -c "put "\""$EXCEL_FILE"\"" "\""$UPLOAD_EXCEL_FILENAME"\"""
+
+TRANSFER_RESULT=$?
+
+if (( $TRANSFER_RESULT != 0 )); then
+  echo "ERROR: An error occurred transferring Excel spreadsheet to Samba"
+  exit 1
+fi
