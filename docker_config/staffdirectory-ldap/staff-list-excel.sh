31a9602d13cbb344dd96ed7b458ea94823634456
David P. Steelman <dsteelma@umd.edu>
2021-08-26 08:35:46 -0400

    LIBITD-1976. Added additional environment variables for Samba upload
    Added the following environment variables to the "staff-list-excel.sh"
    script:
    
    * SMB_HOST - the Samba host to upload the Excel file to
    * SMB_DIRECTORY - the Samba directory to upload the Excel file to
    * UPLOAD_EXCEL_FILENAME - the name of the uploaded file
    * SMB_DO_UPLOAD - controls whether the upload should occur
    
    These environment variables were added to allow for flexibility in
    the test/qa/prod environments.
    
    https://issues.umd.edu/browse/LIBITD-1976

--- docker_config/staffdirectory-ldap/staff-list-excel.sh
+++ docker_config/staffdirectory-ldap/staff-list-excel.sh
@@ -5,16 +5,24 @@
 #
 # The following environment variables are expected by this script:
 #
+#    SMB_HOST - The Samba host (typically '//librfs001v.ad.umd.edu/department$')
+#    SMB_DIRECTORY - The Samba directory to upload the file to
 #    SMB_USER - the Samba username
 #    SMB_PASSWORD - the Samba password
 #    EXCEL_MODIFICATION_USER - The user to set on the Excel spreadsheet
-#    EXCEL_MODIFICATION_PASSWORD - The password to set on the Excel spreadsheet 
+#    EXCEL_MODIFICATION_PASSWORD - The password to set on the Excel spreadsheet
+#    UPLOAD_EXCEL_FILENAME - The name of the uploaded file
+#    SMB_DO_UPLOAD - "true" if the upload should be performed. Any other value prevents upload
 
-export SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-export CONFIG_PROPERTIES_FILE=$SCRIPT_DIR/config/config.properties
-JSON_FILE="$SCRIPT_DIR/output/persons.json"
-EXCEL_FILE="$SCRIPT_DIR/output/All Staff List New.xlsx"
-UPLOAD_EXCEL_FILENAME="TEST-All Staff List New-TEST.xlsx"
+if [ -z "$SMB_HOST" ]; then
+  echo "Please provide a non-empty 'SMB_HOST' environment variable"
+  exit 1
+fi
+
+if [ -z "$SMB_DIRECTORY" ]; then
+  echo "Please provide a non-empty 'SMB_DIRECTORY' environment variable"
+  exit 1
+fi
 
 if [ -z "$SMB_USER" ]; then
   echo "Please provide a non-empty 'SMB_USER' environment variable"
@@ -36,6 +44,16 @@ if [ -z "$EXCEL_MODIFICATION_PASSWORD" ]; then
   exit 1
 fi
 
+if [ -z "$UPLOAD_EXCEL_FILENAME" ]; then
+  echo "Please provide a non-empty 'UPLOAD_EXCEL_FILENAME' environment variable"
+  exit 1
+fi
+
+export SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+export CONFIG_PROPERTIES_FILE=$SCRIPT_DIR/config/config.properties
+JSON_FILE="$SCRIPT_DIR/output/persons.json"
+EXCEL_FILE="$SCRIPT_DIR/output/all-staff-list-new.xlsx"
+
 echo === Building Excel spreadsheet ===
 $SCRIPT_DIR/bin/all-staff-list-builder --user "$EXCEL_MODIFICATION_USER" --password "$EXCEL_MODIFICATION_PASSWORD" --config "$CONFIG_PROPERTIES_FILE" --input "$JSON_FILE" --output "$EXCEL_FILE"
 BUILD_RESULT=$?
@@ -45,11 +63,16 @@ if (( $BUILD_RESULT != 0 )); then
   exit 1
 fi
 
+if [ $SMB_DO_UPLOAD != "true" ]; then
+  echo === Skipping Excel upload -- SMB_DO_UPLOAD is \'$SMB_DO_UPLOAD\' ===
+  exit 0
+fi
+
 echo === Uploading updated Excel spreadsheet ===
 /usr/bin/smbclient \
-  '//librfs001v.ad.umd.edu/department$' \
+  "$SMB_HOST" \
   --user $SMB_USER%$SMB_PASSWORD \
-  -D 'LibraryShare\Staff Directory Information' \
+  -D "$SMB_DIRECTORY" \
   -c "put "\""$EXCEL_FILE"\"" "\""$UPLOAD_EXCEL_FILENAME"\"""
 
 TRANSFER_RESULT=$?
