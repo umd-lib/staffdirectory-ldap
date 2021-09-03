9fa1c8e3a75f9aad75c1ba60bc66b24f12c97a42
David P. Steelman <dsteelma@umd.edu>
2021-09-03 13:37:21 -0400

    LIBITD-1983. Updated "staff-list-drupal.sh" script in Docker config
    Updated "docker_config/staffdirectory-ldap/staff-list-drupal.sh"
    to actually run the "drupal-builder" script.
    
    https://issues.umd.edu/browse/LIBITD-1983

--- docker_config/staffdirectory-ldap/staff-list-drupal.sh
+++ docker_config/staffdirectory-ldap/staff-list-drupal.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
-# Placeholder script for uploading Staff Directory information to Drupal
+# Builds the JSON file used by Drupal and uploads it to the Drupal REST
+# endpoint.
+#
+# The following environment variables are expected by this script:
 
-echo === Updating Drupal ===
+export SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+export CONFIG_PROPERTIES_FILE=$SCRIPT_DIR/config/config.properties
+JSON_FILE="$SCRIPT_DIR/output/persons.json"
+DRUPAL_OUTPUT_FILE="$SCRIPT_DIR/output/drupal.json"
+
+
+echo === Building JSON file for Drupal ===
+
+$SCRIPT_DIR/bin/drupal-builder --config "$CONFIG_PROPERTIES_FILE" --input "$JSON_FILE" --output "$DRUPAL_OUTPUT_FILE"
+BUILD_RESULT=$?
+if (( $BUILD_RESULT != 0 )); then
+  echo "ERROR: An error occurred running drupal-builder."
+  echo $SCRIPT_DIR/bin/drupal-builder --config "$CONFIG_PROPERTIES_FILE" --input "$JSON_FILE" --output "$DRUPAL_OUTPUT_FILE"
+  exit 1
+fi
+
+echo === Uploading $DRUPAL_OUTPUT_FILE to Drupal ===
 echo TODO
